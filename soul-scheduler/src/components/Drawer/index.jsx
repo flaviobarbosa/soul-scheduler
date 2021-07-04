43897ab44e2a8fe0dc62e2d716965becbfa364d5
Flávio M Barbosa <flavio.barbosa@dataprev.gov.br>
2021-07-04 14:53:32 -0300

    improving drawer

--- soul-scheduler/src/components/Drawer/index.jsx
+++ soul-scheduler/src/components/Drawer/index.jsx
@@ -1,4 +1,9 @@
-import {RightOutlined} from "@ant-design/icons";
+import {
+  RightOutlined,
+  LogoutOutlined,
+  CalendarOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
 import {Backdrop} from "../Backdrop/Backdrop";
 import styles from "./styles.module.css";
 
@@ -7,11 +12,38 @@ export const Drawer = ({open, onClose}) => {
     <>
       {open && <Backdrop />}
       <div className={`${styles.container} ${open ? styles.open : ""}`} onClick={onClose}>
-        <nav className={styles.sideDrawer}>
-          <span>
-            <RightOutlined />
-          </span>
-        </nav>
+        <div className={styles.drawer}>
+          <header className={styles.drawerHeader}>
+            <span className={styles.close} onClick={onClose}>
+              <RightOutlined />
+            </span>
+            <div className={styles.userInfo}>
+              <h3 className={styles.userName}>Zeca</h3>
+              <span className={styles.useRoom}>2 - 505</span>
+              <div className={styles.logout}>
+                <span className={styles.logoutIcon}>
+                  <LogoutOutlined />
+                </span>
+                <span>SAIR</span>
+              </div>
+            </div>
+          </header>
+          <div className={`${styles.divider} ${styles.transparent}`}></div>
+          <nav className={styles.links}>
+            <p className={`${styles.link}  ${styles.active}`}>
+              <span className={styles.linkIcon}>
+                <CalendarOutlined />
+              </span>
+              Meus agendamentos
+            </p>
+            <p className={styles.link}>
+              <span className={styles.linkIcon}>
+                <PlusOutlined />
+              </span>
+              Novo agendamento
+            </p>
+          </nav>
+        </div>
       </div>
     </>
   );
